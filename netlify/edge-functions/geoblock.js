08c34a4f3c6f90901122ef406755f40345e1e6c2
andrizan <putra7a@gmail.com>
2022-12-07 11:52:53 +0700

    add geoblock

---
+++ netlify/edge-functions/geoblock.js
@@ -0,0 +1,20 @@
+export default async function geoBlock(req, { geo }) {
+  if (
+    geo.country.code !== 'ID'
+  ) {
+    return;
+  }
+
+  const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <body>
+        Sorry, we are unavailable in ${geo.country.name}
+      </body>
+    </html>
+  `;
+
+  return new Response(html, {
+    headers: { 'content-type': 'text/html' },
+  });
+}
